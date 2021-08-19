44fc841c8de8d3b52ea3ca26bd7a38c316779a11
Inazuma110 <c011703534@edu.teu.ac.jp>
2021-08-19 15:08:44 +0900

    feat: リモ達一覧取得API

---
+++ app/Http/Controllers/API/RemotatsuController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Remotatsu;
+use Illuminate\Http\Request;
+
+class RemotatsuController extends Controller
+{
+    public function index()
+    {
+        return Remotatsu::all();
+    }
+
+}
